a988884c8e622ed204abb4ee55f2534ed561408b
Fabiamz <gustavojoaquimfabiam@gmail.com>
2025-11-14 19:30:32 -0300

    Projeto SA

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/Cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/Login")
+def login():
+    return render_template("login.html")
+
+@app.route("/about")
+def about():
+    return render_template("sobre.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/produtos")
+def produtos():
+    return render_template("produtos.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
